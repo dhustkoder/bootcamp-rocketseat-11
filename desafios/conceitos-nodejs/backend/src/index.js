fddb7f20641f11dba92e9e7c214a8ff7d3d17692
Rafael Moura <rafaelmoura.dev@gmail.com>
2020-12-29 18:34:36 -0300

    rev

--- desafios/conceitos-nodejs/backend/src/index.js
+++ desafios/conceitos-nodejs/backend/src/index.js
@@ -2,4 +2,19 @@ const express = require('express');
 
 const app = express();
 
+/*
+* main http methods
+* GET   -> fetch  info from backend
+* POST  -> create info on backend
+* PUT   -> full update of some resource
+* PATCH -> partial update of some resource
+*/
 
+app.get('/', (request, response) => {
+    return response.send({data: ['hello', 'world']});
+});
+
+
+app.listen(3333, () => {
+    console.log('server is up 👍👍');
+});
