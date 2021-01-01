82b308360b4d1f901b28967c72ae437aa6bc4810
Rafael Moura <rafaelmoura.dev@gmail.com>
2021-01-01 13:11:27 -0300

    impl routes with no persistent storage

--- desafios/conceitos-nodejs/backend/src/index.js
+++ desafios/conceitos-nodejs/backend/src/index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-
-const app = express();
-
 /*
 * main http methods
 * GET   -> fetch  info from backend
@@ -10,6 +6,31 @@ const app = express();
 * PATCH -> partial update of some resource
 */
 
+/*
+* main restful parameter types
+* 
+* query: filters, pagination
+* path:  specify the resource 
+* body:  body of data for the endpoint to inspect
+*/
+
+
+
+const { request } = require('express');
+const express = require('express');
+const { uuid } = require('uuidv4');
+
+const app = express();
+
+
+
+
+app.use(express.json())
+
+
+const projects = [];
+
+
 app.get('/', (request, response) => {
     return response.send({data: ['hello', 'world']});
 });
@@ -17,23 +38,52 @@ app.get('/', (request, response) => {
 
 // projects resource
 app.get('/projects', (request, response) => {
-    console.log('get projects called');
-    return response.send(['project 1', 'project 2']);
+    console.log('get projects called with query params: ', request.query);
+    return response.send(projects);
 });
 
 app.post('/projects', (request, response) => {
-    console.log('create project called');
-    return response.send(['project 1', 'project 2']);
+    console.log('create project called with body: ', request.body);
+    const { title, owner } = request.body;
+    const project = { title, owner, id: uuid() };
+    projects.push(project);
+    return response.send(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-    console.log('update project called');
-    return response.send(['project 1', 'project 2']);
+    const { params, body } = request;
+    console.log('update project called with: ', {params, body});
+   
+    const { title, owner } = body;
+
+    const project = {
+        title,
+        owner,
+        id: params.id
+    };
+
+
+    const index = projects.findIndex(p => p.id == project.id);
+
+    if (index == -1)
+        return response.status(400).send({error: 'Project not found'});
+    
+    projects[index] = project;
+
+    return response.send(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-    console.log('delete project called');
-    return response.send(['project 1', 'project 2']);
+    const {id} = request.params;
+    console.log('delete project called with path params: ', request.params);
+
+    const index = projects.findIndex(p => p.id == id);
+    if (index == -1)
+        return response.status(400).send({error: 'Project not found'});
+    
+    const deletedProject = projects.splice(index, 1);
+
+    return response.status(204).send();
 })
 
 
